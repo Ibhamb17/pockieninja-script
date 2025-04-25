f8b737a3745729afa3a897b2737a65aaf539f6b2
Ibhamb17 <bathsyiibham@gmail.com>
2025-04-25 09:33:11 +0700

    update preset-skill-manager

--- preset-skill-manager.js
+++ preset-skill-manager.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         preset-skill-manage
+// @name         preset-skill-manager
 // @namespace    http://tampermonkey.net/
 // @version      1.0
 // @description  Kelola skill preset otomatis + custom preset dengan dropdown jutsu bergambar (Base Language : Indonesia)
